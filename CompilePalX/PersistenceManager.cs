9cba6427f2e570f15264ec4efbf4cff2ab551869
ruarai <ruarai.big@gmail.com>
2016-12-05 12:02:27 +1100

    use json instead of csv for map file persistence

--- CompilePalX/PersistenceManager.cs
+++ CompilePalX/PersistenceManager.cs
@@ -1,19 +1,29 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.IO;
+using System.Windows.Documents;
+using Newtonsoft.Json;
 
 namespace CompilePalX
 {
     static class PersistenceManager
     {
-        private static string mapFiles = "mapfiles.csv";
+        private static string mapFiles = "mapfiles.json";
         public static void Init()
         {
             if (File.Exists(mapFiles))
-                CompilingManager.MapFiles = new ObservableCollection<string>(File.ReadAllLines(mapFiles));
+            {
+                var list = JsonConvert.DeserializeObject<List<string>>(File.ReadAllText(mapFiles));
+
+                CompilingManager.MapFiles = new ObservableCollection<string>(list);
+            }
 
             CompilingManager.MapFiles.CollectionChanged +=
-                delegate { File.WriteAllLines(mapFiles, CompilingManager.MapFiles); };
+                delegate
+                {
+                    File.WriteAllText(mapFiles, JsonConvert.SerializeObject(CompilingManager.MapFiles,Formatting.Indented));
+                };
         }
     }
 }
