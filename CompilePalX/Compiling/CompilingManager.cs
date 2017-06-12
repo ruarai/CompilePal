c6c10fcdf16b7bd4cec2b3854c57aa23a09395e2
Ruarai <Ruarai@DESKTOP-8R7NMAS>
2017-06-12 12:44:18 +1000

    add missing colon

--- CompilePalX/Compiling/CompilingManager.cs
+++ CompilePalX/Compiling/CompilingManager.cs
@@ -136,7 +136,7 @@ namespace CompilePalX
                     }
 
                     int mapMaxSeverity = map.Errors.Max(e => e.Severity);
-                    CompilePalLogger.LogLineColor("{0} errors/warnings logged for {1}", Error.GetSeverityBrush(mapMaxSeverity), map.Errors.Count, map.MapName);
+                    CompilePalLogger.LogLineColor("{0} errors/warnings logged for {1}:", Error.GetSeverityBrush(mapMaxSeverity), map.Errors.Count, map.MapName);
 
                     var distinctErrors = map.Errors.GroupBy(e => e.ID);
                     foreach (var errorList in distinctErrors)
