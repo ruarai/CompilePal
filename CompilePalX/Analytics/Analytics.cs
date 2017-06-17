3ff9ec10bfca26416c596dcae8cec70edb33d9a3
Ruarai <Ruarai@DESKTOP-8R7NMAS>
2017-06-18 01:11:53 +1000

    change anonymised id generation for more accurate counting of users

--- CompilePalX/Analytics/Analytics.cs
+++ CompilePalX/Analytics/Analytics.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
@@ -20,8 +21,7 @@ namespace CompilePalX
         public static bool Enabled = true;
         static AnalyticsManager()
         {
-            //anonymise the machine name so it's not too stalkery
-            anonymousUserID = GetHashString(Environment.MachineName);
+            anonymousUserID = getUniqueComputerID();
 
             userProperties = new Segment.Model.Properties()
                             {
@@ -32,10 +32,35 @@ namespace CompilePalX
             options = new Options {Context = {{"direct", true}}};
         }
 
+        //Returns a unique, anonymised ID that should allow for more accurate counting of number of users
+        //This method is probably overkill, but MachineName is too common across different computers
+        static string getUniqueComputerID()
+        {
+            string id = Environment.MachineName;
+
+            try
+            {
+                ManagementClass mc = new ManagementClass("win32_processor");
+                var cpus = mc.GetInstances();
+                foreach (var cpu in cpus)//Just for all those people with two cpus
+                    id += cpu.Properties["processorID"].Value.ToString();
+            }
+            catch { }
+            try
+            {
+                ManagementObject disk = new ManagementObject("win32_logicaldisk.deviceid=\"C:\"");
+                disk.Get();
+                id += disk["VolumeSerialNumber"].ToString();
+            }
+            catch { }
+            
+            return GetHashString(id);
+        }
+
         public static byte[] GetHash(string inputString)
         {
             HashAlgorithm algorithm = SHA256.Create();
-            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString + "random as string that makes it secure like"));
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString + "ssaalltt"));
         }
 
         public static string GetHashString(string inputString)
