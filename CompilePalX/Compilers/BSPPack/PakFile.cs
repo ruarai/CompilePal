1ba16a16d37d0d09361f2207a7d8f6363eeed74f
Zach <zachmatuson@gmail.com>
2020-02-11 18:02:19 -0500

    fixed some internal compile pal errors not showing up in error summary

--- CompilePalX/Compilers/BSPPack/PakFile.cs
+++ CompilePalX/Compilers/BSPPack/PakFile.cs
@@ -264,7 +264,7 @@ namespace CompilePalX.Compilers.BSPPack
                 catch (Exception e)
                 {
 	                ExceptionHandler.LogException(e, false);
-	                CompilePalLogger.LogLineColor($"Failed to read file {externalPath}", Brushes.Red);
+	                CompilePalLogger.LogCompileError($"Failed to read file {externalPath}", new Error($"Failed to read file {externalPath}", ErrorSeverity.Error));
 	                return;
                 }
 
@@ -297,7 +297,7 @@ namespace CompilePalX.Compilers.BSPPack
             string externalPath = FindExternalFile(internalPath);
             if (externalPath == String.Empty)
             {
-				CompilePalLogger.LogLineColor($"Failed to find particle manifest file {internalPath}", Brushes.Red);
+				CompilePalLogger.LogCompileError($"Failed to find particle manifest file {internalPath}", new Error($"Failed to find particle manifest file {internalPath}", ErrorSeverity.Error));
 				return;
             }
 
@@ -316,7 +316,7 @@ namespace CompilePalX.Compilers.BSPPack
             }
             else
             {
-				CompilePalLogger.LogLineColor($"Failed to find particle manifest file {internalPath}", Brushes.Red);
+				CompilePalLogger.LogCompileError($"Failed to find particle manifest file {internalPath}", new Error($"Failed to find particle manifest file {internalPath}", ErrorSeverity.Error));
 				return;
             }
         }
